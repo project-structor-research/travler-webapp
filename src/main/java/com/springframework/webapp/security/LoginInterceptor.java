394d18736af86ac1f35db27267cde9c003588f7c
haeyonghahn <yong80211@gmail.com>
2021-12-04 13:25:34 +0900

    interceptor 구현

--- src/main/java/com/springframework/webapp/security/LoginInterceptor.java
+++ src/main/java/com/springframework/webapp/security/LoginInterceptor.java
@@ -2,6 +2,7 @@ package com.springframework.webapp.security;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -9,20 +10,16 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		String email = null;
-		String password = null;
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
 
-		if(email == null && password == null) {
-			response.sendRedirect("/forgot");
+		if("".equals(email) && "".equals(password)) {
+			response.sendRedirect("/login");
 			return false;
 		}
-		// session 처리
-		return false;
-//		System.err.println("Session Create #######");
-//		HttpSession session = request.getSession(true);
-//		session.setAttribute("userEmail", email);
-//		session.setAttribute("userPassword", password);
-//		response.sendRedirect(ip + "/forgot");
-//		return false;
+		HttpSession session = request.getSession(true);
+		session.setAttribute("email", email);
+		
+		return super.preHandle(request, response, handler);
 	}	
 }
